47d5b95c79f111de2ce49c825b0983bbf4423f48
Bunyamin Genc <bunyaming@uia.no>
2023-07-31 19:26:56 +0200

    register

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     },
   },
   plugins: [],
+  //  ? 
 };
 
 //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
