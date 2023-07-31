7e441f01d617e2df6f119560acc3c9c4479cb237
Bunyamin Genc <bunyaming@uia.no>
2023-07-31 19:16:47 +0200

    theme extend structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  // ! kendi theme ayarlarimiz eklemek icin bu kismi kullaniyoruz
   theme: {
     extend: {
       colors: {
