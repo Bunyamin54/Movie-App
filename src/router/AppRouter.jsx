a9fa634af583d5539e7daa13a111026fc0a4e06c
Bunyamin Genc <bunyaming@uia.no>
2023-07-31 20:19:49 +0200

    browser routes route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return <BrowserRouter>
+  
+  <Navbar/>
+  <Routes>
+  <Route path = "/" element = {<Main/>} />
+  
+
+
+
+
+  </Routes>
+  
+  
+  
+  
+  </BrowserRouter>
 };
 
 export default AppRouter;
