21963aba3305e174ca09f64801bb36e5749d0b4b
Bunyamin Genc <bunyaming@uia.no>
2023-07-31 20:28:44 +0200

    App router

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,70 +1,94 @@
-import React from "react";
-import GoogleIcon from "../assets/icons/GoogleIcon";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import avatar from "../assets/icons/avatar.png";
 
-const Register = () => {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
   return (
-    <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
-      <div
-        className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
+    <>
+      <Disclosure
+        as="nav"
+        className="bg-neutral-100 dark:bg-gray-900 py-3 dark:text-white fixed top-0 z-20 w-full"
       >
-        <form className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]">
-          <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
-            Sign Up
-          </h2>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              className="peer"
-              name="floating_text"
-              type="text"
-              placeholder=" "
-              required
-            />
-            <label htmlFor="floating_text">First Name</label>
+        <div className="mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between">
+            <a className="pr-2 text-2xl font-semibold" href="#">
+              React Movie App
+            </a>
+            <div className="absolute inset-y-0 right-0 flex items-center ">
+              {/* Profile dropdown */}
+              <Menu as="div" className="relative ml-3">
+                <div>
+                  <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={avatar}
+                      alt="user"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Register
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Login
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Log out
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
           </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              className="peer"
-              name="floating_text"
-              type="text"
-              placeholder=" "
-              required
-            />
-            <label htmlFor="floating_text">Last Name</label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              className="peer"
-              name="floating_email"
-              type="email"
-              placeholder=" "
-              required
-            />
-            <label htmlFor="floating_email">Email</label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              className="peer"
-              name="floating_password"
-              type="password"
-              placeholder=" "
-              required
-            />
-            <label htmlFor="floating_password">Password</label>
-          </div>
-          <button type="submit" className="btn-danger">
-            Register
-          </button>
-          <button
-            type="button"
-            className="btn-danger flex justify-between items-center"
-          >
-            Continue with Google
-            <GoogleIcon color="currentColor" />
-          </button>
-        </form>
-      </div>
-    </div>
+        </div>
+      </Disclosure>
+      <div className="h-[55px]"></div>
+    </>
   );
-};
-
-export default Register;
+}
