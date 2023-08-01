37d20e7023ca721c69b5dcad6ca5c9ca192fc134
Bunyamin Genc <bunyaming@uia.no>
2023-08-01 22:08:40 +0200

    switch component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu, Switch, Transition } from "@headlessui/react";
 import avatar from "../assets/icons/avatar.png";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
@@ -26,6 +26,7 @@ export default function Navbar() {
               {currentUser && (
                 <h5 className="mr-2 capitalize">{currentUser?.displayName}</h5>
               )}
+              <Switch/>
               {/* Profile dropdown */}
               <Menu as="div" className="relative ml-3">
                 <div>
